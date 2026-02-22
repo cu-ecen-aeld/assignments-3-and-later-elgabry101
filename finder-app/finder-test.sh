87c623642bc4635ea45435566855ea18a2be29ab
elgabry101 <elgabry2112001@gmail.com>
2026-02-22 10:05:05 +0200

    fixing my finder app

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp
-username=$(cat conf/username.txt)
+username=$(cat etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -62,6 +62,7 @@ rm -rf /tmp/aeld-data
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
+echo "$OUTPUTSTRING" > /tmp/assignment4-result.txt 
 if [ $? -eq 0 ]; then
 	echo "success"
 	exit 0
