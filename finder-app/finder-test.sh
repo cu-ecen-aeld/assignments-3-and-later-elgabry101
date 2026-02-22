bd76ecb6d540164e229804fa71f1c16ba7ddefc3
elgabry101 <elgabry2112001@gmail.com>
2026-02-22 07:50:06 +0200

    updating finder test to write to /tmp/assignment4-result.txt

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -7,7 +7,7 @@ set -u
 
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
+WRITEDIR=/tmp
 username=$(cat conf/username.txt)
 
 if [ $# -lt 3 ]
@@ -22,7 +22,7 @@ then
 else
 	NUMFILES=$1
 	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+	WRITEDIR=/tmp/$3
 fi
 
 
